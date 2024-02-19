cb2b05aaa221a03524a12b94008c731a762f10b6
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-02-19 21:49:55 +0530

    Time: 177 ms (5.09%) | Memory: 54.9 MB (14.88%) - LeetSync

--- 650-2-keys-keyboard/2-keys-keyboard.java
+++ 650-2-keys-keyboard/2-keys-keyboard.java
@@ -1,17 +1,22 @@
 class Solution {
-        public int minSteps(int n) {
-        int[] dp = new int[n+1];
-
-        for (int i = 2; i <= n; i++) {
-            dp[i] = i;
-            for (int j = i-1; j > 1; j--) {
-                if (i % j == 0) {
-                    dp[i] = dp[j] + (i/j);
-                    break;
-                }
-                
-            }
+    public int minSteps(int n) {
+        int dp[][]=new int[n+1][n+1];
+        for(int[] ele:dp){
+            Arrays.fill(ele,-1);
         }
-        return dp[n];
+        if(n==1) return 0;
+        return 1+findit(n,dp,1,1,1);
+    }
+    public int findit(int n, int[][] dp, int str, int cpy, int val){
+        if(str==n && val<=n) return 0; // check the base case correctly
+        if(str<n && val<n){
+            if(dp[str][cpy]==-1){
+                int val1=findit(n,dp,str+cpy,cpy,val+1); // pass val as a parameter
+                int val2=findit(n,dp,str,str,val+1); // pass val as a parameter
+                dp[str][cpy]=1+Math.min(val1,val2);
+            }
+            return dp[str][cpy];
+        }
+        return n+1; // return a large enough value
     }
 }
