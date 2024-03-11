34ab030a086031219ec9c71b886ed3cacc48a045
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-03-11 11:27:59 +0530

    Time: 88 ms (9.67%) | Memory: 46.2 MB (31.30%) - LeetSync

--- 868-push-dominoes/push-dominoes.java
+++ 868-push-dominoes/push-dominoes.java
@@ -1,83 +1,32 @@
 class Solution {
     public String pushDominoes(String dominoes) {
-        char[] dos = dominoes.toCharArray();
-        int left = 'L';
-        int i = 0;
-        while(i<dos.length){
-            if(dos[i] == '.'){
-                int j = i;
-                while(j<dos.length && dos[j] == '.'){
-                    j++;
+        char[] str=dominoes.toCharArray();
+        StringBuilder s=new StringBuilder();
+        boolean hasChange=true;
+        while(hasChange){
+            HashSet<Integer> hashR=new HashSet<>();
+            HashSet<Integer> hashL=new HashSet<>();
+            hasChange=false;
+            for(int i=0;i<str.length;i++){
+                if(str[i]=='R' && ((i<str.length-2 && str[i+1]=='.' && str[i+2]!='L') || (i==str.length-2 && str[i+1]=='.'))){
+                    hashR.add(i+1);
+                    hasChange=true;
                 }
-                int right = j>=dos.length? 'R': dos[j];
-                if(right == left){
-                    while(i < j){
-                        dos[i++] = (char)right;
-                    }
-                }else if(right == 'R'){
-                    i = j-1;
-                }else{
-                    j--;
-                    while(i < j){
-                        dos[i++] = (char)left;
-                        dos[j--] = (char)right;
-                    }
+                else if(str[i]=='L' && ((i>1 && str[i-1]=='.' && str[i-2]!='R')||(i==1 && str[i-1]=='.'))){
+                    hashL.add(i-1);
+                    hasChange=true;
                 }
-                left = right;
-            }else{
-                left = dos[i];
             }
-            i++;
-        }
-        return new String(dos);
-    }
-    public String pushDominoes1(String dominoes) {
-        StringBuilder sb = new StringBuilder();
-        char[] dos = dominoes.toCharArray();
-        Stack<Character> stack = new Stack<>();
-        int lastRight = -1;
-        int lastLeft = -1;
-
-        for(int i = 0; i<dos.length;){
-            if(dos[i] == 'R'){
-                while(i<dos.length && dos[i] != 'L'){
-                    stack.push('R');
-                    if(dos[i] == 'R'){
-                        lastRight = i;
-                    }
-                    i++;
-                }
-            }else if(dos[i] == 'L'){
-                int numOfPop = 0;
-                if(lastLeft == -1 && lastRight == -1){
-                    numOfPop = i;
-                }else{
-                    numOfPop = lastLeft > lastRight? (i- lastLeft):(i-lastRight)/2;
-                }
-                int count = numOfPop;
-                while(count > 0){
-                    stack.pop();
-                    count--;
-                }
-                if(lastRight != -1 && lastRight > lastLeft && (i-lastRight)%2 == 0 && numOfPop >= 1) {
-                    stack.push('.');
-                    numOfPop--;
-                }
-                while(count < numOfPop){
-                    stack.push('L');
-                    count++;
-                }
-                stack.push('L');
-                lastLeft = i;
-                i++;
-            }else{
-                stack.push(dos[i]);
-                i++;
+            for(int ele:hashR){
+                str[ele]='R';
+            }
+            for(int ele:hashL){
+                str[ele]='L';
             }
         }
-        while(!stack.isEmpty()){
-            sb.append(stack.pop());
+        for(int i=0;i<str.length;i++){
+            s.append(str[i]);
         }
-        return sb.reverse().toString();
+        return s.toString();
     }
 }
