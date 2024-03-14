0059b03510013ed8aaf6d936e3486c834d65a96f
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-03-14 11:03:17 +0530

    Time: 1233 ms (10.58%) | Memory: 47.5 MB (67.77%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.java
@@ -3,7 +3,7 @@ class Solution {
         int count=0;
         for(int i=0;i<=nums.length-1;i++){
             int sum=0;
-            for(int j=i;j<=nums.length-1;j++){
+            for(int j=i;j<=nums.length-1 && sum<=goal;j++){
                 sum+=nums[j];
                 if(sum==goal) count++;
             }
