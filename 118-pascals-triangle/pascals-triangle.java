e9bd1c7e0a904948d3f2d23524e7996a18b2a61e
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-01 09:50:59 +0530

    Time: 60 ms (85.79%) | Memory: 45.2 MB (8.43%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,23 +1,20 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> list = new ArrayList<>();
+        List<List<Integer>> list=new ArrayList<>();
         list.add(new ArrayList<>());
         list.get(0).add(1);
-
-        for (int i = 1; i < numRows; i++) {
-            List<Integer> row = new ArrayList<>();
-            List<Integer> prevRow = list.get(i - 1);
-            row.add(1); // First element is always 1
-
-            for (int j = 1; j < i; j++) {
-                // Each element is the sum of the two elements above it
-                row.add(prevRow.get(j - 1) + prevRow.get(j));
+        for(int i=1;i<numRows;i++){
+            list.add(new ArrayList<>());
+            for(int j=0;j<=i;j++){
+                if(j==0 || j==i){
+                    list.get(i).add(1);
+                }
+                else{
+                    list.get(i).add(list.get(i-1).get(j)+list.get(i-1).get(j-1));
+                    System.out.println(list);
+                }
             }
-
-            row.add(1); // Last element is always 1
-            list.add(row);
         }
-
         return list;
     }
 }
