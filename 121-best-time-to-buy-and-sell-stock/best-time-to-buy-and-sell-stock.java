263d565c0109890abc7d1f4e4afea2c06a782673
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-03 08:19:59 +0530

    Time: 2 ms (78.46%) | Memory: 62 MB (19.53%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max=0;
+        int prev=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(max<(prices[i]-prev)){ 
+                max=prices[i]-prev;
+            }
+            if(prices[i]<prev){
+                prev=prices[i];
+            }
+        }
+        return max;
+    }
+}
