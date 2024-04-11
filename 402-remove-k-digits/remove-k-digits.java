38577721a6c6acfb02de829b8f463bc7b14a3355
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-11 12:46:17 +0530

    Time: 32 ms (29.80%) | Memory: 50.3 MB (5.18%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.java
+++ 402-remove-k-digits/remove-k-digits.java
@@ -1,15 +1,23 @@
 class Solution {
     public String removeKdigits(String num, int k) {
         if(k >= num.length()) return "0";
-        PriorityQueue<int[]> ptr = new PriorityQueue<>((a, b) -> (b[0] - a[0]));
+       PriorityQueue<String> ptr = new PriorityQueue<>((a, b) -> {
+            int digitA = a.charAt(0) - '0';
+            int digitB = b.charAt(0) - '0';
+            return Integer.compare(digitB, digitA);
+        });
+        
         char[] arr = num.toCharArray();
         for(int i = 0; i < arr.length; i++){
-            while(!ptr.isEmpty() && (ptr.peek()[0]) > (arr[i] - '0') && k > 0){ 
-                int[] itr = ptr.poll();
-                arr[itr[1]] = '*';
+            while(!ptr.isEmpty() && (ptr.peek().charAt(0) - '0') > (arr[i] - '0') && k > 0){ 
+                String itr = ptr.poll();
+                // Correctly parse the index part of the string
+                int index = Integer.parseInt(itr.substring(1));
+                arr[index] = '*';
                 k--;
             }
-            ptr.offer(new int[]{arr[i]-'0',i});
+            // Store the digit and index as a string, ensuring index is correctly represented
+            ptr.offer(arr[i] + String.valueOf(i));
         }
         for(int i = arr.length - 1; i >= 0 && k > 0; i--){
             if(arr[i] != '*'){ 
