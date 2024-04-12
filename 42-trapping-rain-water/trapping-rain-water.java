2f1b0a908d44ea0e3f9473f040e59e6e02ab9a26
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-12 16:51:12 +0530

    Time: 91 ms (6.67%) | Memory: 46.5 MB (23.43%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,21 +1,36 @@
 class Solution {
     public int trap(int[] height) {
-        int n = height.length;
-        int ans[] = new int[n];
-        int max = 0;
-        for(int i=n-1;i>=0;i--)
-        {
-            if(max<height[i]) max = height[i];
-            ans[i] = max;
+        if(height.length <= 2) return 0;
+        int maxl = height[0];
+        int maxr = 0;
+        int i = 1;
+        int index = 1; // Initialize index to 1 instead of 0
+        int sum = 0;
+        while(i < height.length - 1) {
+            if(maxl <= height[i]){
+                maxl = height[i];
+                i++;
+            } else {
+                if(index <= i){
+                    index = i + 1;
+                    maxr = height[index];
+                    for(int j = index + 1; j < height.length; j++){ // Use for-loop for clarity
+                        if(height[j] > maxr){
+                            maxr = height[j];
+                            index = j;
+                        }
+                    }
+                }
+                
+                if(maxr > height[i]) {
+                    sum += Math.min(maxl, maxr) - height[i];
+                    i++;
+                } else {
+                    // If no higher bar is found on the right, move the pointer to the next element
+                    i++;
+                }
+            }
         }
-        max = 0;
-        int sol = 0;
-        for(int i=0;i<n;i++)
-        {   if(max<height[i]) max = height[i];
-            sol+= Math.min(max,ans[i])-height[i];
-        }
-
-        return sol
-        ;
+        return sum;
     }
 }
