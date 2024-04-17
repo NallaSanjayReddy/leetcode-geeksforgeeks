68d1f17fdb2e236a92219e009e5b5ee4c1624240
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-17 13:18:40 +0530

    Time: 27 ms (29.70%) | Memory: 44.9 MB (71.31%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -3,18 +3,23 @@ class Solution {
         int sum=0;
         Arrays.sort(piles);
         if(h==piles.length) return piles[piles.length-1];
-        int l = 1;
-        int r = piles[piles.length - 1];
-        while (l < r) { // Change this to l < r to ensure we don't miss the correct answer
-            int mid = l + (r - l) / 2;
-            long val = 0;
-            for (int pile : piles) {
-                val += (long) Math.ceil((double) pile / mid);
+        int l=1;
+        int r=piles[piles.length-1];
+        int mid=0;
+        while(l<r){
+            mid=l+(r-l)/2;
+            long val=0;
+            for(int j=0;j<piles.length;j++){
+                if(val>h){
+                    break;
+                }
+                val+=(long)Math.ceil((double)piles[j]/mid);
             }
-            if (val > h) {
-                l = mid + 1; // If we need more hours than h, increase the speed
-            } else {
-                r = mid; // If we can finish within h hours, try to see if there's a slower speed possible
+            if(val>h){
+                l=mid+1;
+            }
+            else{
+                r=mid;
             }
         }
         return l;
