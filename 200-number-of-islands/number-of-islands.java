43c29cab6e67d9f54150df12e8c238455f276e03
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-19 09:29:25 +0530

    Time: 4 ms (49.39%) | Memory: 49.3 MB (57.87%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,38 +1,31 @@
 class Solution {
-    private final int[] delRow = {1, -1, 0, 0};
-    private final int[] delCol = {0, 0, -1, 1};
-
     public int numIslands(char[][] grid) {
-        int island = 0;
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] vis = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] == '1' && vis[i][j] == 0) {
-                    island++;
-                    dfs(grid, vis, n, m, i, j);
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        int numIslands = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    numIslands++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        return island;
+        
+        return numIslands;
     }
-
-    private void dfs(char[][] grid, int[][] vis, int n, int m, int row, int col) {
-        if (row < 0 || row >= n || col < 0 || col >= m || vis[row][col] == 1) {
+    
+    private void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] != '1') {
             return;
         }
-
-        vis[row][col] = 1;
-
-        for (int i = 0; i < 4; i++) {
-            int nRow = row + delRow[i];
-            int nCol = col + delCol[i];
-
-            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] == '1' && vis[nRow][nCol] == 0) {
-                dfs(grid, vis, n, m, nRow, nCol);
-            }
-        }
+        
+        grid[i][j] = '0'; // mark as visited
+        dfs(grid, i + 1, j); // down
+        dfs(grid, i - 1, j); // up
+        dfs(grid, i, j + 1); // right
+        dfs(grid, i, j - 1); // left
     }
 }
