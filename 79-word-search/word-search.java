ed8ba40d728dc6f3edd61adc7e61893b82602a3b
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-27 08:49:17 +0530

    Time: 150 ms (42.54%) | Memory: 40.9 MB (98.41%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,42 +1,28 @@
-public class Solution {
+class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        int n = board[0].length;
-
-        boolean[][] visited = new boolean[m][n];
-        boolean result = false;
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word.charAt(0)) {
-                    result = backtrack(board, word, visited, i, j, 0);
-                    if (result) return true;
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++){
+            for(int j = 0; j < board[0].length; j++){
+                if(findit(board, word, 0, i, j, visited)) {
+                    return true;
                 }
             }
         }
-        
         return false;
     }
-    
-    private boolean backtrack(char[][] board, String word, boolean[][] visited, int i, int j, int index) {
-        if (index == word.length()) {
-            return true;
-        }
-        
-        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) {
-            return false;
-        }
-        
-        visited[i][j] = true;
-        
-        if (backtrack(board, word, visited, i + 1, j, index + 1) ||
-            backtrack(board, word, visited, i - 1, j, index + 1) ||
-            backtrack(board, word, visited, i, j + 1, index + 1) ||
-            backtrack(board, word, visited, i, j - 1, index + 1)) {
-            return true;
-        }
-        
-        visited[i][j] = false;
-        return false;
+
+    private boolean findit(char[][] board, String word, int index, int i, int j, boolean[][] visited) {
+        if(index == word.length()) return true;
+        if(i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(index)) return false;
+
+        visited[i][j] = true; // mark as visited
+
+        boolean exists = findit(board, word, index + 1, i + 1, j, visited)
+                      || findit(board, word, index + 1, i - 1, j, visited)
+                      || findit(board, word, index + 1, i, j + 1, visited)
+                      || findit(board, word, index + 1, i, j - 1, visited);
+
+        visited[i][j] = false; // unmark as visited
+        return exists;
     }
 }
