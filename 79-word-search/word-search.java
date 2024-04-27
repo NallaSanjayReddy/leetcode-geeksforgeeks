29c86e0caa2dd30c9d9d80d634fd002e1926fc1a
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-04-27 09:02:47 +0530

    Time: 801 ms (5.00%) | Memory: 44.9 MB (14.96%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -6,7 +6,7 @@ class Solution {
             for (int j = 0; j < board[0].length; j++) {
                 if (board[i][j] == word.charAt(0)) {
                     StringBuilder str = new StringBuilder();
-                    ans |= findit(board, word, 0, i, j, str, visited);
+                    ans = findit(board, word, 0, i, j, str, visited);
                     if (ans) {
                         return true;
                     }
@@ -17,29 +17,19 @@ class Solution {
     }
 
     public boolean findit(char[][] board, String word, int k, int i, int j, StringBuilder str, boolean[][] visited) {
-        if (visited[i][j]) return false;
-        str.append(board[i][j]);
-        if (k == word.length() - 1) {
-            if (str.toString().equals(word)) return true;
-            str.deleteCharAt(str.length() - 1); // backtrack
+        if (k == word.length()) {
+            return true;
+        }
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || visited[i][j] || board[i][j] != word.charAt(k)) {
             return false;
         }
         visited[i][j] = true;
-        boolean ans = false;
-        int[] rows = {-1, 1, 0, 0};
-        int[] cols = {0, 0, -1, 1};
-        for (int d = 0; d < 4; d++) {
-            int newRow = i + rows[d];
-            int newCol = j + cols[d];
-            if (newRow >= 0 && newRow < board.length && newCol >= 0 && newCol < board[0].length) {
-                if (word.charAt(k + 1) == board[newRow][newCol]) {
-                    ans |= findit(board, word, k + 1, newRow, newCol, new StringBuilder(str), visited);
-                    if (ans) break; // stop if one valid path is found
-                }
-            }
-        }
+        str.append(board[i][j]);
+        boolean ans = findit(board, word, k + 1, i - 1, j, new StringBuilder(str), visited) ||
+                      findit(board, word, k + 1, i + 1, j, new StringBuilder(str), visited) ||
+                      findit(board, word, k + 1, i, j - 1, new StringBuilder(str), visited) ||
+                      findit(board, word, k + 1, i, j + 1, new StringBuilder(str), visited);
         visited[i][j] = false; // backtrack
-        str.deleteCharAt(str.length() - 1); // backtrack
         return ans;
     }
 }
