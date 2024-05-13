e07fef9603ba9affe55adda052be698ac536fef2
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-05-13 16:52:37 +0530

    Time: 4 ms (100.00%) | Memory: 44.7 MB (28.93%) - LeetSync

--- 860-design-circular-queue/design-circular-queue.java
+++ 860-design-circular-queue/design-circular-queue.java
@@ -1,97 +1,68 @@
-class MyCircularQueue {
-    Node head=null;
-    Node tail=null;
-
-    int currentLength=0;
-    int totalLength=0;
-    public MyCircularQueue(int k) {
-        head=new Node(-1);
-        tail=new Node(-1);
-
-        head.next=tail;
-        head.prev=tail;
-
-        tail.next=head;
-        tail.prev =head;
-
-        totalLength=k;
+class Node {
+    int val;
+    Node next;
+    Node prev;
+    Node(int data){
+        val=data;
+        next=null;
+        prev=null;
     }
-    
-    public boolean enQueue(int value) {
-        if(isFull()){
-            return false;
-        }
-
-        Node left = head.prev;
-        Node right= head;
-
-        Node temp = new Node(value);
-
-        temp.next= right;
-
-        right.prev = temp;
-
-        left.next=temp;
-
-        temp.prev=left;
-
-        currentLength++;
-
-        
-
-        return true;
-    }
-    
-    public boolean deQueue() {
-        if(isEmpty()){
-            return false;
-        }
-
-        Node left = tail;
-        Node right= tail.next.next;
- 
-       left.next = right;
-
-       right.prev = left;
-
-
-        currentLength--;
-
-        return true;
-    }
-    
-    public int Front() {
-        if(currentLength<=0){
-            return -1;
-        }
-
-        return tail.next.val;
-    }
-    
-    public int Rear() {
-        if(currentLength<=0){
-            return -1;
-        }
-
-        return head.prev.val;
-    }
-    
-    public boolean isEmpty() {
-        return currentLength==0 ? true : false;
-    }
-    
-    public boolean isFull() {
-        return currentLength==totalLength ? true : false;
-    }
-
 }
 
-class Node{
-    int val;
-    Node next=null;
-    Node prev=null;
+class MyCircularQueue {
+    Node first, rear;
+    int length, cur;
 
-    Node(int val){
-        this.val=val;
+    public MyCircularQueue(int k) {
+        length = k;
+        cur = 0;
+        first = new Node(-1); // Dummy node for the front
+        rear = new Node(-1); // Dummy node for the rear
+        first.next = rear;
+        first.prev = rear;
+        rear.next = first;
+        rear.prev = first;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) return false;
+        Node newNode = new Node(value);
+        if (isEmpty()) {
+            first.next = newNode;
+            rear.prev = newNode;
+            newNode.prev = first;
+            newNode.next = rear;
+        } else {
+            newNode.prev = rear.prev;
+            rear.prev.next = newNode;
+            rear.prev = newNode;
+            newNode.next = rear;
+        }
+        cur++;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) return false;
+        first.next = first.next.next;
+        first.next.prev = first;
+        cur--;
+        return true;
+    }
+
+    public int Front() {
+        return isEmpty() ? -1 : first.next.val;
+    }
+
+    public int Rear() {
+        return isEmpty() ? -1 : rear.prev.val;
+    }
+
+    public boolean isEmpty() {
+        return cur == 0;
+    }
+
+    public boolean isFull() {
+        return cur == length;
     }
 }
