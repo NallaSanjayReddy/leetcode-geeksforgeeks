bc623c081ed4ef419db470d695ffafeb7a3955a1
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-06-06 10:48:27 +0530

    Time: 9 ms (99.76%) | Memory: 44.9 MB (93.23%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.java
+++ 876-hand-of-straights/hand-of-straights.java
@@ -1,21 +1,33 @@
+import java.util.Arrays;
+
 class Solution {
-
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if (hand.length % groupSize != 0) return false;
-
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        for (int card : hand) hm.put(card, hm.getOrDefault(card, 0) + 1);
-
-        Arrays.sort(hand);
-        for (int card : hand) {
-            if (hm.get(card) <= 0) continue;
-            for (int i = 1; i < groupSize; i++) {
-                int count = hm.getOrDefault(card + i, 0);
-                if (count > 0) hm.put(card + i, count - 1); else return false;
-            }
-            hm.put(card, hm.get(card) - 1);
+        if (hand == null || hand.length == 0 || hand.length % groupSize != 0) {
+            return false; // Return false if the hand is null, empty, or not divisible by groupSize
         }
 
-        return true;
+        Arrays.sort(hand); // Sort the array to group consecutive numbers
+
+        for (int i = 0; i < hand.length; i++) {
+            if (hand[i] != -1) { // Check if the card has not been used in a group
+                int count = 1; // Start with the current card
+                int prev = hand[i]; // The previous card in the group
+                hand[i] = -1; // Mark the card as used
+
+                for (int j = i + 1; j < hand.length && count < groupSize; j++) {
+                    if (hand[j] == prev + 1) { // Check if the next card is consecutive
+                        prev = hand[j]; // Update the previous card
+                        hand[j] = -1; // Mark the card as used
+                        count++; // Increment the count of cards in the group
+                    }
+                }
+
+                if (count != groupSize) {
+                    return false; // If the group is not of the correct size, return false
+                }
+            }
+        }
+
+        return true; // If all cards can be grouped correctly, return true
     }
 }
