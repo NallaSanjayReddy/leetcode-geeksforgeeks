a49dd384789f51570a57594a1ed931b26e2a1040
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-06-09 21:34:11 +0530

    Time: 35 ms (60.45%) | Memory: 43.3 MB (30.22%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,21 +1,15 @@
 class Solution {
     public int numSquares(int n) {
-        List<Integer> list=new ArrayList<Integer>(); 
-        for(int i=n;i>0;i--){
-            if(Math.floor(Math.sqrt(i))==Math.ceil(Math.sqrt(i))){
-                list.add(i);
-            }
-        }
-        int[] dp=new int[n+1];
-        Arrays.fill(dp,-1);
-        return findit(n,dp,list);
-    }
-    int findit(int n, int[] dp,List<Integer> list){
-        if(n==0) return 0;
-        if(dp[n]==-1){
-            dp[n]=10001;
-            for(int i=list.size()-1; i>=0 && list.get(i)<=n;i--){
-                dp[n]=Math.min(dp[n],1+findit(n-list.get(i),dp,list));
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, n);
+        dp[0] = 0;
+
+        for(int target = 0; target < n + 1; target++){
+            for(int s = 0; s < target; s++){
+                int square = s*s;
+                if(target - square < 0)
+                    break;
+                dp[target] = Math.min(dp[target], 1 + dp[target - square]);    
             }
         }
         return dp[n];
