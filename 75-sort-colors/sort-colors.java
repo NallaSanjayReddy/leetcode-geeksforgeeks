817f13c946998ee689882fa914d207ac3506abf3
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-06-12 10:46:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (42.89%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,20 +1,35 @@
 class Solution {
-  public void sortColors(int[] nums) {
-    int l = 0;
-    int r = nums.length - 1;
-
-    for (int i = 0; i <= r;)
-      if (nums[i] == 0)
-        swap(nums, i++, l++);
-      else if (nums[i] == 1)
-        ++i;
-      else
-        swap(nums, i, r--);
-  }
-
-  private void swap(int[] nums, int i, int j) {
-    final int temp = nums[i];
-    nums[i] = nums[j];
-    nums[j] = temp;
-  }
+    public void sortColors(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+        while(left<nums.length && nums[left]==0){
+            left++;
+        }
+        while(right>=0 && nums[right]==2){
+            right--;
+        }
+        int i=left;
+        while(i<=right){
+            if(nums[i]==0){
+                nums[i]=nums[left];
+                nums[left]=0;
+                left++;
+                while(left<nums.length && nums[left]==0){
+                    left++;
+                }
+                i=left;
+            }
+            else if(nums[i]==2){
+                nums[i]=nums[right];
+                nums[right]=2;
+                right--;
+                while(right>=0 && nums[right]==2){
+                    right--;
+                }
+            }
+            else{
+                i++;
+            }
+        }
+    }
 }
