83b452268ef6b45d169dcb6d0b90419b98a42303
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-07-13 11:53:13 +0530

    Time: 382 ms (11.99%) | Memory: 77.6 MB (13.07%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,26 +1,26 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
-        int[] d = new int[n + 1];
-        d[1] = nums[0];
-        int size = 1;
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] > d[size]) {
-                d[++size] = nums[i];
-            } else {
-                int left = 1, right = size;
-                while (left < right) {
-                    int mid = (left + right) >> 1;
-                    if (d[mid] >= nums[i]) {
-                        right = mid;
-                    } else {
-                        left = mid + 1;
-                    }
-                }
-                int p = d[left] >= nums[i] ? left : 1;
-                d[p] = nums[i];
-            }
+        int[][] dp = new int[n + 1][n + 1];
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
         }
-        return size;
+        return findit(nums, dp, 0, -1);
+    }
+
+    public int findit(int[] nums, int[][] dp, int i, int prevIndex) {
+        if (i == nums.length) {
+            return 0;
+        }
+        if (dp[i][prevIndex + 1] != -1) {
+            return dp[i][prevIndex + 1];
+        }
+        int notTake = findit(nums, dp, i + 1, prevIndex);
+        int take = 0;
+        if (prevIndex == -1 || nums[i] > nums[prevIndex]) {
+            take = 1 + findit(nums, dp, i + 1, i);
+        }
+        dp[i][prevIndex + 1] = Math.max(take, notTake);
+        return dp[i][prevIndex + 1];
     }
 }
