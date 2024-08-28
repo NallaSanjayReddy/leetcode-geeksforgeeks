49b26948500875167e0008f1aa4d0f1b428751b0
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-08-28 21:40:10 +0530

    Time: 19 ms (89.93%) | Memory: 85.1 MB (37.03%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.java
+++ 2035-count-sub-islands/count-sub-islands.java
@@ -1,41 +1,29 @@
 class Solution {
     public int countSubIslands(int[][] grid1, int[][] grid2) {
-        int m = grid2.length;
-        int n = grid2[0].length;
         int count = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < grid2.length; i++) {
+            for (int j = 0; j < grid2[0].length; j++) {
                 if (grid2[i][j] == 1) {
-                    if (dfs(grid1, grid2, i, j)) {
+                    if (findIsland(grid1, grid2, i, j)) {
                         count++;
                     }
                 }
             }
         }
-
         return count;
     }
 
-    private boolean dfs(int[][] grid1, int[][] grid2, int i, int j) {
-        int m = grid2.length;
-        int n = grid2[0].length;
-
-        if (i < 0 || i >= m || j < 0 || j >= n || grid2[i][j] == 0) {
-            return true; // Out of bounds or water cell
+    private boolean findIsland(int[][] grid1, int[][] grid2, int i, int j) {
+        if (i >= 0 && i < grid2.length && j >= 0 && j < grid2[0].length && grid2[i][j] == 1) {
+            if (grid1[i][j] == 0) return false;
+            grid2[i][j] = 0;
+            return (
+                findIsland(grid1, grid2, i, j + 1) &
+                findIsland(grid1, grid2, i, j - 1) &
+                findIsland(grid1, grid2, i + 1, j) &
+                findIsland(grid1, grid2, i - 1, j)
+            );
         }
-
-        if (grid1[i][j] == 0) {
-            return false; // Mismatched cell
-        }
-
-        grid2[i][j] = 0; // Mark cell as visited
-
-        boolean up = dfs(grid1, grid2, i - 1, j);
-        boolean down = dfs(grid1, grid2, i + 1, j);
-        boolean left = dfs(grid1, grid2, i, j - 1);
-        boolean right = dfs(grid1, grid2, i, j + 1);
-
-        return up && down && left && right;
+        return true;
     }
 }
