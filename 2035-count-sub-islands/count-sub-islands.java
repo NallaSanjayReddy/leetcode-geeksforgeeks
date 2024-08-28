4d3a52411c5decb04ec8aadeddd61d2e743e5345
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-08-28 21:41:53 +0530

    Time: 20 ms (70.90%) | Memory: 85.8 MB (28.82%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.java
+++ 2035-count-sub-islands/count-sub-islands.java
@@ -17,12 +17,10 @@ class Solution {
         if (i >= 0 && i < grid2.length && j >= 0 && j < grid2[0].length && grid2[i][j] == 1) {
             if (grid1[i][j] == 0) return false;
             grid2[i][j] = 0;
-            return (
-                findIsland(grid1, grid2, i, j + 1) &
-                findIsland(grid1, grid2, i, j - 1) &
+            return 
+                findIsland(grid1, grid2, i, j + 1) & findIsland(grid1, grid2, i, j - 1) &
                 findIsland(grid1, grid2, i + 1, j) &
-                findIsland(grid1, grid2, i - 1, j)
-            );
+                findIsland(grid1, grid2, i - 1, j);
         }
         return true;
     }
