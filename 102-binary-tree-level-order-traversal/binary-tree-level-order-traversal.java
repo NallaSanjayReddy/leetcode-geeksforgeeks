d4fd88a872c791c8bd2ade05963f3e8b59117cd3
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-09-29 14:29:56 +0530

    Time: 1 ms (90.43%) | Memory: 44.8 MB (76.74%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -14,24 +14,29 @@
  * }
  */
 class Solution {
+
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> list=new ArrayList<>();
-        findit(root,0,list);
-        return list;
-    }
-    public void findit(TreeNode root, int height, List<List<Integer>> list){
-        if(root!=null){
-            if(list.size()==(height)){
-                list.add(new ArrayList<>());
-                list.get(height).add(root.val);
-                findit(root.left,height+1,list);
-                findit(root.right,height+1,list);
+        List<List<Integer>> res = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        if (root == null) return res;
+
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                TreeNode curr = queue.poll();
+                level.add(curr.val);
+                if (curr.left != null) {
+                    queue.add(curr.left);
+                }
+                if (curr.right != null) {
+                    queue.add(curr.right);
+                }
             }
-            else{
-                list.get(height).add(root.val);
-                findit(root.left,height+1,list);
-                findit(root.right,height+1,list);
-            }
+            res.add(level);
         }
+        return res;
     }
 }
