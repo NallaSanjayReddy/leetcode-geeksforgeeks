91fdbecec2dac6d2e895b411c2b285e163c4fd78
NALLA SANJAY REDDY <59687492+NallaSanjayReddy@users.noreply.github.com>
2024-11-16 12:39:46 +0530

    Time: 2 ms (77.43%) | Memory: 62 MB (18.47%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -3,12 +3,10 @@ class Solution {
         int max=0;
         int prev=Integer.MAX_VALUE;
         for(int i=0;i<prices.length;i++){
-            if(max<(prices[i]-prev)){ 
+            if(max<prices[i]-prev){
                 max=prices[i]-prev;
             }
-            if(prices[i]<prev){
-                prev=prices[i];
-            }
+            if(prices[i]<prev) prev=prices[i];
         }
         return max;
     }
